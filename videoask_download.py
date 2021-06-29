f9e2bfb8d1099e92162ea5cb466e42a4f343dd41
David Kong <davidnkong@gmail.com>
2021-06-29 12:01:59 -0400

    Do some cleanup and clearer commenting, take input args, etc.

--- videoask_download.py
+++ videoask_download.py
@@ -3,14 +3,14 @@ import subprocess
 import json
 from random import randint
 import time
+import sys
 
-# Note: these tokens expire every 15 minutes, so you pretty much always have to refresh it
-
-token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UTkJRamhFUmpZd05VRXlRakpFUkRGRk5rSXpPRGc0T0RZMlFqWTNSamd3TURoRVFUVTROZyJ9.eyJodHRwczovL3ZpZGVvYXNrLmNvbS9sb2dpbnNfY291bnQiOjI1LCJpc3MiOiJodHRwczovL2F1dGgudmlkZW9hc2suY29tLyIsInN1YiI6ImF1dGgwfDYwNjM3YmIyM2U4ZTJhMDA3MGE3OTg0MSIsImF1ZCI6WyJodHRwczovL2FwaS52aWRlb2Fzay5jb20vIiwiaHR0cHM6Ly92aWRlb2Fzay5ldS5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjI0OTE1MjM1LCJleHAiOjE2MjQ5MTYxMzUsImF6cCI6InAzTW0zOGpSaWRlaFNNTU9BOTdsVHZKMjdQQ25uR0poIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCJ9.Cv34B0LBEEpUnpyW6NwJUVFzKLHLxTtjt_r47AXsbsWtAVhg26RM_8KKjnAfz2M6-uq-U2tXo4zGwzizGTzKZyahKD6KkqJvBmmfFeOFNv4h0bbUW7Dgx5xqo3nMAfjsMq-iNelUYUp2vG8bCKZNe9cF586uTe3EdMtd_kbtB6BQImf8GuYuKmGuFvK24bsw3xspSz8HzqHn9XPmCTFom2qmjxl2NrrxdZuTh4ll4m8ggWrWAQKst76jSs6LN4HwXJkpQgl2enT1cGHLl2MNqxNXzFpo4pxhswiKejtWA6_qajMPtQBNw7mW3lOA9G7YXuEsjLweE1GfiNVKHcd61g"
-form_id = "93acb357-9b95-43db-95b0-acd47aa44a3f"
-
+# config
+download_location_prefix = "" # leave blank to download to current directory. Must have a trailing slash	
 
 # calls the VideoAsk API to download all of the videos for a given question
+# skips audio answers for now
+# TODO: also handle audio answers
 def downloadQuestion(questionID, questionNumber, token):
 	print(f'requesting data for question {questionID}')
 
@@ -25,31 +25,44 @@ def downloadQuestion(questionID, questionNumber, token):
 		print ("skipping because of an error")
 		return
 
-	jsonResult = json.loads(result.stdout)
+	questionDict = json.loads(result.stdout)
 
+
+	# loop through answers to this question
 	counter = 1
+	for item in questionDict['results']:
 
-	for item in jsonResult['results']:
+		# if we don't know their name, we need a unique placeholder to avoid overwriting
 		if ('contact_name' in item):
 			name = item['contact_name']
 		else:
-			name = str(randint(1000, 9999))
+			name = str(randint(1000, 9999)) 
 
+		# skip if it isn't a media answer
 		if ('media_url' in item):
 			if (item['media_type'] == "video"):
-				url = item['media_url']
 
 				print(f'Downloading {name}')
 
 				# download the file
-				r = requests.get(url, allow_redirects=True) 
-				open(f'{name}_{questionID}_{counter}.mp4', 'wb').write(r.content)
+				r = requests.get(item['media_url'], allow_redirects=True) 
+				# save it to disk
+				open(f'{download_location_prefix}{name}_{questionID}_{counter}.mp4', 'wb').write(r.content)
 
 		counter += 1
-		time.sleep(0.2)
+		time.sleep(0.3) # avoid tripping the rate limiter
 
 
 if __name__ == "__main__":
+
+	print(len(sys.argv))
+	if (len(sys.argv) != 3):
+		print("Error: you need to pass in two args: the form ID, and the API token.")
+		quit()
+
+	# Note: these tokens expire every 15 minutes, so you pretty much always have to refresh it
+	form_id = sys.argv[1]
+	token = sys.argv[2]
 
 	print("retrieving the form data")
 	# grab the form
@@ -59,15 +72,15 @@ if __name__ == "__main__":
 
 	print(form.stderr)
 	# print(form)
-	jsonResult = json.loads(form.stdout)
+	formDict = json.loads(form.stdout)
 
 	# loop through the questions in the form
 	questionNumber = 1
-	for question in jsonResult['questions']:
+	for question in formDict['questions']:
 		print ("question " + question['question_id'])
 
 		# call downloadQuestion with this question
 		downloadQuestion(question['question_id'], questionNumber, token)
 		questionNumber += 1
-		time.sleep(0.2)
+		time.sleep(0.3) # avoid tripping the rate limiter
 
